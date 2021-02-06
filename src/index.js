158992d55dd228d4425cecfdd46dd4595f7f6959
Edward Stoner <edwardlstoner@gmail.com>
2021-02-06 13:05:10 -0700

    Add error handling

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const router = new express.Router();
 const port = process.env.PORT;
 const cors = require("cors");
 const Traffic = require("./traffic");
-const { getCameraData } = require("./apiCalls");
+const { getCameraData, getTravelTimeData } = require("./apiCalls");
 require("./mongoose.js");
 
 const app = express();
@@ -11,18 +11,35 @@ app.use(express.json());
 app.use(cors());
 
 router.get("/photos", async (req, res) => {
-  const data = await getCameraData();
-  res.send(JSON.stringify(data));
+  try {
+    const data = await getCameraData();
+    res.send(JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/speed", async (req, res) => {
+  try {
+    const data = await getTravelTimeData();
+    res.send(JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.get("/historical", async (req, res) => {
-  let data = await Traffic.find({});
-  data = data.map((d) => ({
-    timeStamp: d.timeStamp,
-    travelTime: d.travelTime,
-    direction: d.direction,
-  }));
-  res.send(JSON.stringify(data));
+  try {
+    let data = await Traffic.find({});
+    data = data.map((d) => ({
+      timeStamp: d.timeStamp,
+      travelTime: d.travelTime,
+      direction: d.direction,
+    }));
+    res.send(JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.use(router);
